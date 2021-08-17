bf9c097915a0e4e784e731db42dbc49042066aa2
Анна Михайлова <annamihajlova@farfique.local>
2021-08-17 10:32:31 +0300

    Refactoring: App moved to a separate component

---
+++ src/components/app/app.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
+import ItemStatusFilter from '../item-status-filter';
+import './app.css';
+
+
+const App = () => {
+
+const data = [
+  {label:"Drink Coffee", important: false, id: 1},
+  {label:"Build React App", important: true, id: 2},
+  {label:"Have a lunch", important: false, id: 3},  
+]
+
+
+
+  return (
+    <div>
+      <AppHeader />
+      <div id="panel" className="block_margin">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>     
+      <TodoList todos={data} />
+    </div>
+);
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
-import ItemStatusFilter from './components/item-status-filter';
+import App from './components/app';
 import './styles.css';
 
 
-
-
-
-
-const App = () => {
-
-const data = [
-  {label:"Drink Coffee", important: false, id: 1},
-  {label:"Build React App", important: true, id: 2},
-  {label:"Have a lunch", important: false, id: 3},  
-]
-
-
-
-  return (
-    <div>
-      <AppHeader />
-      <div id="panel" className="block_margin">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div>     
-      <TodoList todos={data} />
-    </div>
-);
-};
 
 ReactDOM.render(<App />, document.getElementById("root"));
