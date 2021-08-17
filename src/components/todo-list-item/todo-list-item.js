e1da2fa1b11c165030454199d41bdf305ab1766e
Анна Михайлова <annamihajlova@farfique.local>
2021-08-17 11:18:35 +0300

    Refactoring: class components

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,34 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './todo-list-item.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faExclamation} from '@fortawesome/free-solid-svg-icons';
 import {faTrash} from '@fortawesome/free-solid-svg-icons';
 
-const TodoListItem = ({label, important = false}) => {
-const style = {
-    color: important? 'steelblue' : 'black',
-    fontWeight: important? 'bold' : 'normal'
-}
-    return (
-        <span className="todo-list-item">
-            <span 
-                style={style}
-                className="todo-list-item-label">
-                { label }
-            </span>
-            <button 
-                type="button"
-                className="btn btn-outline-danger btn-sm">
-                <FontAwesomeIcon icon={faTrash} />
-            </button>
-            <button 
-                type="button"
-                className="btn btn-outline-success btn-sm">
-                <FontAwesomeIcon icon={faExclamation} />
-            </button>
-        </span>
-        
-    );
-};
+export default class TodoListItem extends Component {
+    
+    render(){
+        const {label, important = false} = this.props;
 
-export default TodoListItem;
+        const style = {
+            color: important? 'steelblue' : 'black',
+            fontWeight: important? 'bold' : 'normal'
+        }
+
+        return (
+            <span className="todo-list-item">
+                <span 
+                    style={style}
+                    className="todo-list-item-label">
+                    { label }
+                </span>
+                <button 
+                    type="button"
+                    className="btn btn-outline-danger btn-sm">
+                    <FontAwesomeIcon icon={faTrash} />
+                </button>
+                <button 
+                    type="button"
+                    className="btn btn-outline-success btn-sm">
+                    <FontAwesomeIcon icon={faExclamation} />
+                </button>
+            </span>
+            
+        );
+    }
+}
